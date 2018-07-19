32e1dbc7ac67f5327ba656fedd2897fa31de0389
Diego Muracciole <diegomuracciole@gmail.com>
2018-07-19 02:39:03 -0300

    Fix duplicate variable

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import ReactPDF from '@react-pdf/react-pdf'
 
-import InlineStyle from 'styled-components/src/models/InlineStyle'
-import StyledNativeComponent from 'styled-components/src/models/StyledNativeComponent'
-import constructWithOptions from 'styled-components/src/constructors/constructWithOptions'
+import _InlineStyle from 'styled-components/src/models/InlineStyle'
+import _StyledNativeComponent from 'styled-components/src/models/StyledNativeComponent'
+import _constructWithOptions from 'styled-components/src/constructors/constructWithOptions'
 import css from 'styled-components/src/constructors/css'
 import ThemeProvider from 'styled-components/src/models/ThemeProvider'
 import withTheme from 'styled-components/src/hoc/withTheme'
 import isStyledComponent from 'styled-components/src/utils/isStyledComponent'
 
-const constructWithOptions = constructWithOptions(css)
-const InlineStyle = InlineStyle(ReactPDF.StyleSheet)
-const StyledNativeComponent = StyledNativeComponent(constructWithOptions, InlineStyle)
+const constructWithOptions = _constructWithOptions(css)
+const InlineStyle = _InlineStyle(ReactPDF.StyleSheet)
+const StyledNativeComponent = _StyledNativeComponent(constructWithOptions, InlineStyle)
 const styled = (tag: Target) => constructWithOptions(StyledNativeComponent, tag)
 
 const aliases = `Image Text View Link Page Document`
